87d1c07edde0a2a915cd65d5e539b993d2a74d0c
Igor Rewers <igor.rewers@uczen.zsk.poznan.pl>
2025-01-14 14:27:56 +0100

    Plyty desktopowa

---
+++ c#/Desktopowe/desktopowa/plyty/plyty/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace plyty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::plyty.Properties.Resources.obraz;
+            this.pictureBox3.Location = new System.Drawing.Point(175, 97);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(408, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 76);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(413, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 46);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(415, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(530, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(175, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 31);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(217)))), ((int)(((byte)(24)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(354, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::plyty.Properties.Resources.obraz2;
+            this.button2.Location = new System.Drawing.Point(915, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 70);
+            this.button2.TabIndex = 9;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::plyty.Properties.Resources.obraz3;
+            this.button3.Location = new System.Drawing.Point(31, 227);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 70);
+            this.button3.TabIndex = 10;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(1061, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "Form1";
+            this.Text = "Twoje dzwieki wykonal xiega";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ c#/Desktopowe/desktopowa/plyty/plyty/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace plyty
+{
+    public partial class Form1 : Form
+    {
+        int currentIndex = 0;
+        int len = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Pliki.getData();
+            Pliki.setData();
+            len = Pliki.setLenOfProperties();
+            setContainersOnStart();
+        }
+
+        public void setContainersOnStart()
+        {
+            label1.Text = Pliki.listOfObj[0].artist;
+            label2.Text = Pliki.listOfObj[0].album;
+            label3.Text = (Pliki.listOfObj[0].songsNumber).ToString();
+            label4.Text = Pliki.listOfObj[0].yearOfEdition;
+            label5.Text = (Pliki.listOfObj[0].downloadNumber).ToString();
+        }
+
+        public void setContainers(int value)
+        {
+            label1.Text = Pliki.listOfObj[value].artist;
+            label2.Text = Pliki.listOfObj[value].album;
+            label3.Text = (Pliki.listOfObj[value].songsNumber).ToString();
+            label4.Text = Pliki.listOfObj[value].yearOfEdition;
+            label5.Text = (Pliki.listOfObj[value].downloadNumber).ToString();
+        }
+
+        //dodawanie 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string downloadNumber = label5.Text;
+            int int_downloadNumber = int.Parse(downloadNumber);
+            int_downloadNumber += 1;
+            Pliki.listOfObj[currentIndex].downloadNumber = int_downloadNumber;
+            label5.Text = (Pliki.listOfObj[currentIndex].downloadNumber).ToString();
+        }
+        //prawo
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentIndex += 1;
+            if (currentIndex > len - 1)
+            {
+                currentIndex = 0;
+            }
+            setContainers(currentIndex);
+        }
+        //lewo
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentIndex -= 1;
+            if (currentIndex < 0)
+            {
+                currentIndex = len - 1;
+            }
+            setContainers(currentIndex);
+        }
+    }
+    public partial class Pliki
+    {
+        public string artist;
+        public string album;
+        public int songsNumber;
+        public string yearOfEdition;
+        public int downloadNumber;
+
+        public static List<Pliki> listOfObj = new List<Pliki>();
+        public static List<string> properties = new List<string>();
+
+        public static int lenOfProperties = 0;
+
+        public static void getData()
+        {
+            StreamReader file = new StreamReader("Data.txt");
+            string line = file.ReadLine();
+
+            while (line != null)
+            {
+                if (line != "")
+                {
+                    properties.Add(line);
+                }
+                line = file.ReadLine();
+            }
+        }
+        public static void setData()
+        {
+            for (int i = 0; i < properties.Count; i += 5)
+            {
+                Pliki obj = new Pliki();
+                obj.artist = properties[i];
+                obj.album = properties[i + 1];
+                obj.songsNumber = int.Parse(properties[i + 2]);
+                obj.yearOfEdition = properties[i + 3];
+                obj.downloadNumber = int.Parse(properties[i + 4]);
+                listOfObj.Add(obj);
+            }
+        }
+
+        public static int setLenOfProperties()
+        {
+            lenOfProperties = listOfObj.Count;
+            return lenOfProperties;
+        }
+    }
+}
